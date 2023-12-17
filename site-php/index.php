b55468941f3a3f22c7ddd688ba55a7aff10570ea
Nezgit41 <vgankin858@gmail.com>
2023-12-17 22:11:56 +1200

    добавлен mysql

--- site-php/index.php
+++ site-php/index.php
@@ -1,12 +1,29 @@
 <?php
-// process.php
 
 if (isset($_GET['text']) && isset($_GET['site'])) {
     $inputText = $_GET['text'];
     $site = $_GET['site'];
 
     if (!empty($inputText)) {
-        echo "Received from $site: " . htmlspecialchars($inputText);
+        $servername = "mysql";
+        $username = "admin";
+        $password = "admin";
+        $database = "sample-data";
+
+        $conn = new mysqli($servername, $username, $password, $database);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "INSERT INTO your_table_name (column_name) VALUES ('$inputText')";
+        if ($conn->query($sql) === TRUE) {
+            echo "Received from $site: " . htmlspecialchars($inputText) . " and inserted into MySQL successfully.";
+        } else {
+            echo "Error inserting record: " . $conn->error;
+        }
+
+        $conn->close();
     } else {
         echo "Text is empty.";
     }
